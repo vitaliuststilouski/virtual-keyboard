b07384a842626597cda18f899771d01fb2f5f053
Vitaly Ustsilouski <vitaliustsilouski@gmail.com>
2020-01-22 07:09:48 +0300

    feat: implement keaboard rendering

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // make container for elements
 
-var keyboardArray = ['ё', '1', '2', '3', '4', '5', '6',
-    '7', '8', '9', '10', '-', '+', 'Backspace',
-    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Del',
-    'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '\\', 'Enter',
-    'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#129033', 'Shift',
-    'Ctrl', 'Win', 'Alt', '', 'Alt', '&#129032', '&#129035', '&#129034', 'Ctrl'
+var keyboardArray = [
+    ['ё', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9', '10', '-', '+', 'Backspace'
+    ],
+    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ','\\', 'Del'],
+    ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#129033', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', '', 'Alt', '&#129032', '&#129035', '&#129034', 'Ctrl']
 ];
 
 function makeContainer() {
@@ -34,92 +36,56 @@ function showKeyboard() {
 }
 showKeyboard();
 
-// defineIndex to separate
-
-var getIndex = function () {
-    var allNecessaryIndexes = {};
-    for (var j = 0; j < keyboardArray.length; j++) {
-        if (keyboardArray[j] === 'Backspace') {
-            allNecessaryIndexes['Backspace'] = j;
-        }
-        if (keyboardArray[j] === 'Del') {
-            allNecessaryIndexes['Del'] = j;
-        }
-
-        if (keyboardArray[j] === 'Enter') {
-            allNecessaryIndexes['Enter'] = j;
-        }
-
-        if (keyboardArray[j] === 'Shift') {
-            allNecessaryIndexes['Shift'] = j;
-        }
-        if (keyboardArray[j] === 'Ctrl') {
-            allNecessaryIndexes['Ctrl'] = j;
-        }
-    }
-    return allNecessaryIndexes;
-}
-
 
 function makeKey() {
-    // getNumbers
-    var breakLine = getIndex();
-    var breakValues = [];
-
-    for (const prop in breakLine) {
-        breakValues.push(breakLine[prop])
-    };
-
     //add line
-    var y = 0;
-    while(y < breakValues.length) {
+    for (var i = 0; i < keyboardArray.length; i++) {
         var keyLine = document.createElement('div');
         keyLine.classList.add('line');
-        
-        for (var i = 0; i <= breakValues[y]; i++) {
-
-            var key = document.createElement('div')
+        for (var j = 0; j < keyboardArray[i].length; j++) {
+            var key = document.createElement('div');
             var symbol = document.createElement('span');
-            symbol.innerHTML = keyboardArray[i];
+            symbol.innerHTML = keyboardArray[i][j];
+
             symbol.classList.add('symbol');
             key.append(symbol);
             key.classList.add('keyShape');
-            keyLine.append(key)
-    
-            if (keyboardArray[i] === 'Backspace') {
+            keyLine.append(key);
+
+            if (keyboardArray[i][j] === 'Backspace') {
                 key.classList.add('backspace')
             }
 
-            if (keyboardArray[i] === 'Del') {
+            if (keyboardArray[i][j] === 'Del') {
                 key.classList.add('delete');
             }
-    
-            if (keyboardArray[i] === 'Enter') {
+
+            if (keyboardArray[i][j] === 'Enter') {
                 key.classList.add('enter')
             }
 
-                
-            if (keyboardArray[i] === 'Tab') {
+
+            if (keyboardArray[i][j] === 'Tab') {
                 key.classList.add('tab')
             }
-                
-            if (keyboardArray[i] === 'CapsLock') {
+
+            if (keyboardArray[i][j] === 'CapsLock') {
                 key.classList.add('capslock')
             }
 
-            if (keyboardArray[i] === 'Shift') {
+            if (keyboardArray[i][j] === 'Shift') {
                 key.classList.add('shift-left')
             }
 
-            if (keyboardArray[i] === '') {
+            if (keyboardArray[i][j] === '') {
                 key.classList.add('space')
             }
-
         }
         document.querySelector('.keyboard').append(keyLine);
-        y++;
     }
+
 }
 makeKey();
 
-// console.log(keyboardArray.length)
+
+var button = document.querySelectorAll
